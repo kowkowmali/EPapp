69eb9995a667c2cb1027aca76b59fd12d7314868
kowsarheydari <kowsarheydari82@gmail.com>
2023-06-19 17:58:48 +0330

    Frontend completed

---
+++ src/main/java/com/example/haftsadbist/TelActivity.java
@@ -0,0 +1,14 @@
+package com.example.haftsadbist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tel);
+    }
+}
